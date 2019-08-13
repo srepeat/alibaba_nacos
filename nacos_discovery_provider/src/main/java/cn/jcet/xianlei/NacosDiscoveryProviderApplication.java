1ef764fe1b1ae333ec4ffe956c9ca183a2395a91
DaHey <haha_jcet@163.com>
2019-08-13 15:14:30 +0800

    服务注册问题

---
+++ nacos_discovery_provider/src/main/java/cn/jcet/xianlei/NacosDiscoveryProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.jcet.xianlei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosDiscoveryProviderApplication.class, args);
+	}
+
+}
